eb03f42e55d809e078a57f96cdb50b0b6aca9bae
Raymart Palermo <raymartbat20@gmail.com>
2021-12-28 09:26:41 +0800

    use secure_asset and use npm for production

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
         <!-- Styles -->
         @yield('styles')
     </head>
