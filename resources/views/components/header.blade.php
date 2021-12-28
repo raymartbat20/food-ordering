16794d518d898f549fda2e053d4122dc5891b373
Raymart Palermo <raymartbat20@gmail.com>
2021-12-28 09:21:58 +0800

    install tailwind css

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,12 @@
+<nav class="relative bg-white border-b-2 border-gray-300 text-gray-900">
+    <div class="container mx-auto flex justify-between">
+        <ul class="flex">
+            <li class="hover:bg-blue-800 hover:text-white">
+                <a href="#" class="relative block py-6 px-2 lg:p-6 text-sm lg:text-base font-bold">Order</a>
+            </li>
+            <li class="hover:bg-blue-800 hover:text-white">
+                <a href="#" class="relative block py-6 px-2 lg:p-6 text-sm lg:text-base font-bold">Reports</a>
+            </li>
+        </ul>
+    </div>
+</nav>
