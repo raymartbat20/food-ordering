679d365de0cec17e70f3b22aa4f8ede5ec1e0011
Raymart Palermo <raymartbat20@gmail.com>
2021-12-28 11:20:36 +0800

    fix asset on css

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ config('app.env') === 'local' ? asset('css/app.css') : secure_asset('css/app.css') }}" rel="stylesheet">
         <!-- Styles -->
         @yield('styles')
     </head>
