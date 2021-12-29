4a2898630d04b9e8707eaeb007b376375bba3245
Raymart Palermo <raymartbat20@gmail.com>
2021-12-29 08:27:27 +0800

    add num_format on total price

--- resources/views/order/lists.blade.php
+++ resources/views/order/lists.blade.php
@@ -65,7 +65,7 @@
                                     </td>
                                     <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                         <p class="text-gray-900 whitespace-no-wrap">
-                                            {{ $order->total }}
+                                            {{ number_format($order->total,2,'.',',') }}
                                         </p>
                                     </td>
                                     <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
